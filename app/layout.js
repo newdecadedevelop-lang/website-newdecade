bcfe6b663daeadd6286a475f5f60482a35fdce14
Diego Hernandez <diego_hernandez96@outlook.com>
2026-01-07 20:21:41 -0600

    :zap: Next JS 16

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,16 @@
 import { Kumbh_Sans, Nunito, Outfit,Lora } from 'next/font/google'
-import "/public/assets/css/plugins/bootstrap.min.css"
-import "/public/assets/css/plugins/font-awesome-pro.css"
-import "/public/assets/css/plugins/nice-select.css"
-import "/public/assets/css/plugins/swiper.bundle.css"
-import "/public/assets/icon/font-awesome/css/all.css"
-// import "/public/assets/css/plugins/owl.carousel.min.css"
-import "/public/assets/css/plugins/modal-video.min.css"
-import "/public/assets/css/plugins/mobile.css"
-import "/public/assets/css/plugins/aos.css"
-import "/public/assets/css/typography.css"
-import "/public/assets/css/master.css"
-import "/public/assets/css/plugins/responsive.css"
+import "../public/assets/css/plugins/bootstrap.min.css"
+import "../public/assets/css/plugins/font-awesome-pro.css"
+import "../public/assets/css/plugins/nice-select.css"
+import "../public/assets/css/plugins/swiper.bundle.css"
+import "../public/assets/icon/font-awesome/css/all.css"
+// import "../public/assets/css/plugins/owl.carousel.min.css"
+import "../public/assets/css/plugins/modal-video.min.css"
+import "../public/assets/css/plugins/mobile.css"
+import "../public/assets/css/plugins/aos.css"
+import "../public/assets/css/typography.css"
+import "../public/assets/css/master.css"
+import "../public/assets/css/plugins/responsive.css"
 
 const nunito = Nunito({
     weight: ['300', '400', '500', '600', '700'],
